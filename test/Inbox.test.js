e791a62e75b49c38baa3e851ce4920a85c31f4a2
LeandroViana <LeandroViana1988@gmail.com>
2022-10-13 12:21:19 +0100

    Update unit tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,6 +7,7 @@ const { interface, bytecode} = require('../compile');
 
 let accounts;
 let inbox;
+const INITIAL_MESSAGE = "Initial Message!!";
 beforeEach(async () => {
     //Get a list of all accounts
 
@@ -14,12 +15,24 @@ beforeEach(async () => {
     //User one of those account to deploy
     // the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: ['Hi there!']})
+        .deploy({data: bytecode, arguments: [INITIAL_MESSAGE]})
         .send({from: accounts[0],gas:1000000});
 });
 
 describe('Inbox',() => {
     it('should deploy the contract',()=> {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
     })
+
+    it('should have initial Message',async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message,INITIAL_MESSAGE);
+    });
+
+    it('should can change message',async () => {
+        const newMessage = "AHAHA";
+        await inbox.methods.setMessage(newMessage).send({ from:accounts[0] });
+        const messageFromChain = await inbox.methods.message().call();
+        assert.equal(messageFromChain,newMessage);
+    });
 })
