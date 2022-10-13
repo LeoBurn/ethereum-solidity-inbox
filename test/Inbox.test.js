6f6700959bfb7916057f3ba239c289213c3d6362
LeandroViana <LeandroViana1988@gmail.com>
2022-10-13 11:37:26 +0100

    Deploy Contract on unittest

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,25 @@
-// contract test code will go here
+const assert = require('assert');
+const ganache = require ('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode} = require('../compile');
+
+
+let accounts;
+let inbox;
+beforeEach(async () => {
+    //Get a list of all accounts
+
+    accounts = await web3.eth.getAccounts();
+    //User one of those account to deploy
+    // the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Hi there!']})
+        .send({from: accounts[0],gas:1000000});
+});
+
+describe('Inbox',() => {
+    it('should deploy the contract',()=> {
+        console.log(inbox);
+    })
+})
